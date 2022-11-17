41c95b5f48becb05580335013ce1024f4c11ec1d
Berna <btnaberna@Bernas-Macbook-Air.local>
2022-11-17 17:39:47 +0000

    initial push to check

---
+++ src/test/java/ServerTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class ServerTest {
+
+    Server server;
+
+    @BeforeEach
+    public void setUp(){
+        server = new Server();
+    }
+
+    // TODO: test that guest can only get served if over 18
+    @Test
+    public void canTestIfOver18(){
+
+    }
+
+    // TODO: test that guest can only get served if has enough money to buy a drink (every drink is £5)
+
+    // TODO: test that guest can only get served if sober enough (set sobriety level on guest)
+
+    // TODO: test that guest can only get served if guest is not banned from the pub
+
+    // TODO: test that guest can only get served if guest can pay in local currency (add £ char as currency)
+
+    // EXTENSIONS
+
+    // TODO: test that guest can only get served if server can make favourite drink
+    //  (give server a list of drinks (strings) it can make)
+
+}
